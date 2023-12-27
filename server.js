f26a91bb49bf7a3ba26e4ae502ea7c60c9a11766
Deepak Anto J <deepakanto212@gmail.com>
2023-12-27 20:12:22 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+//const io = require('socket.io')(3000)
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useCreateIndex: true,
+  //useFindAndModify: false,
+};
+
+mongoose.connect('mongodb+srv://deepakanto212:deepak212@cluster0.cgdzxan.mongodb.net/?retryWrites=true&w=majority', mongooseOptions)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error.message);
+  });
+
+  const messageSchema = new mongoose.Schema({
+  name: String,
+  message: String,
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model('Message', messageSchema);
 const users = {}
 
 io.on('connection', socket => {
@@ -7,7 +41,11 @@ io.on('connection', socket => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
   })
-  socket.on('send-chat-message', message => {
+  socket.on('send-chat-message', async (message) => {
+
+    const userName = users[socket.id];
+    const newMessage = new Message({ name: userName, message });
+    await newMessage.save();
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
   })
   socket.on('disconnect', () => {
@@ -15,3 +53,7 @@ io.on('connection', socket => {
     delete users[socket.id]
   })
 })
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
